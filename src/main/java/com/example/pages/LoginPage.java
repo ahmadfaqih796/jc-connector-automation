04e9451dc416f033c44d01e5462355b024cfcdf4
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-05 14:30:27 +0700

    login page

--- src/main/java/com/example/pages/LoginPage.java
+++ src/main/java/com/example/pages/LoginPage.java
@@ -1,5 +1,27 @@
 package com.example.pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class LoginPage {
+   private WebDriver driver;
+   private By usernameInput = By.id("username");
+   private By passwordInput = By.id("password");
+   private By loginButton = By.cssSelector("button[type='submit']");
+
+   public LoginPage(WebDriver driver) {
+      this.driver = driver;
+   }
+
+   public void enterUsername(String username) {
+      driver.findElement(usernameInput).sendKeys(username);
+   }
+
+   public void enterPassword(String password) {
+      driver.findElement(passwordInput).sendKeys(password);
+   }
+
+   public void clickLoginButton() {
+      driver.findElement(loginButton).click();
+   }
 }
